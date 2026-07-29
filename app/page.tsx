3682dce36bfc2848200f6a34d04cae14b9eaca98
predator0xa <aarizzubair789@gmail.com>
2026-07-29 22:49:56 +0530

    Add coming soon launch toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
 import ComingSoon from "@/components/ComingSoon";
 
+import Navbar from "@/components/layout/Navbar";
+import Hero from "@/sections/home/Hero";
+import EditorialGrid from "@/sections/home/EditorialGrid";
+import FeaturedProducts from "@/sections/home/FeaturedProducts";
+import EditorialJournal from "@/sections/home/EditorialJournal";
+import FeaturedOutfits from "@/sections/home/FeaturedOutfits";
+
+const LAUNCH = false;
+
 export default function Home() {
-  return <ComingSoon />;
+  if (!LAUNCH) {
+    return <ComingSoon />;
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <EditorialGrid />
+      <EditorialJournal />
+      <FeaturedOutfits />
+      <FeaturedProducts />
+    </>
+  );
 }
